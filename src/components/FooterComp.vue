749401775040bff5907fb91809f9a1009ef5c7e0
Chany4 <chandreleighdavids04052005@gmail.com>
2024-08-28 01:26:46 -0700

    test

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="footer-container">
+      <div class="footerTop">
+        <div class="row py-4 px-4">
+          <div class="col col-4">
+            <h4 id="h4-foooter">About Symphony</h4>
+            <p id="p"><router-link class="nav-link" to="/about">Brand Story</router-link></p><br>
+            <p id="p"><router-link class="nav-link" to="/home">Symphony Community</router-link></p><br>
+            <p id="p"><router-link class="nav-link" to="/home">Discounts</router-link></p>
+          </div>
+          <div class="col col-4">
+            <h4 id="h4-foooter">Customer Service</h4><br>
+            <p id="p"><router-link class="nav-link" to="/about">FAQ</router-link></p><br>
+            <p id="p"><router-link class="nav-link" to="/contact">Refunds</router-link></p><br>
+            <p id="p"><router-link class="nav-link" to="/home">Promotions</router-link></p><br>
+            <p id="p"><router-link class="nav-link" to="/contact">Contact Us</router-link></p><br>
+            <p id="p"><router-link class="nav-link" to="/about">Terms and Conditions</router-link></p><br>
+            <p id="p"><router-link class="nav-link" to="/home">Membership</router-link></p><br>
+          </div>
+          <div class="col col-4">
+            <h4 id="h4-foooter">News</h4><br>
+            <p id="p"><router-link class="nav-link" to="/home">News</router-link></p><br>
+            <p id="p"><router-link class="nav-link" to="/home">Partnerships</router-link></p><br>
+            <div class="social-icons" id="i">
+              <i class="bi bi-facebook"></i>
+              <i class="bi bi-instagram"></i>
+              <i class="bi bi-pinterest"></i>
+              <i class="bi bi-twitter"></i>
+              <i class="bi bi-tiktok"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      <div class="footerBottom py-3">
+        <div class="row">
+          <div class="col-12 text-center">
+            <p class="lead">
+              Symphony &copy; <span>{{ cYear }}</span> By Chandre Leigh Davids & Shakeel Davids
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "FooterComp",
+    data() {
+      return {
+        cYear: new Date().getUTCFullYear(),
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .footer-container {
+    width: 100%;
+  }
+  
+  .footerTop {
+    background-color: #3a3a3a;
+    color: white;
+    padding: 20px;
+  }
+  
+  .footerBottom {
+    background-color:#000;
+    color: white;
+  }
+  
+  .social-icons {
+    display: flex;
+    gap: 15px;
+  }
+  
+  #p, h4 {
+    text-align: start;
+  }
+  </style>
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,61 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="x">
+        <img src="https://chany4.github.io/capstone-images/images/home%20page/carousel_home.jpg" alt="homePage">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-4">
+        <div class="a">
+          <img src="https://chany4.github.io/capstone-images/images/home%20page/home_pagecar.png" alt="">
+        </div>
+      </div>
+      <div class="col-8">
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  // Your component logic here
 }
 </script>
+
+<style scoped>
+/* Apply a global reset */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box; /* Include padding and border in element's total width and height */
+}
+
+.container-fluid{
+  overflow-x: unsetm; /* Hide horizontal overflow */
+  background-color: black;
+}
+
+.x{
+  width: 1700px;
+  height: min-content;
+}
+
+.x> img {
+  display: block; /* Remove any unwanted space below the image */
+  height: 100%; /* Adjust height as needed */
+  width: 100%; /* Ensure the image takes full viewport width */
+  object-fit: contain; 
+  object-position: center; /* Center the image */
+}
+.a{
+  width: 100%;
+  height: auto;
+}
+.a>img{
+  width: 100%;
+  height: 100%;
+}
+</style>

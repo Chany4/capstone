35be364d94d6b72dc233a4f95f01c09ececc49cc
Chany4 <chandreleighdavids04052005@gmail.com>
2024-09-03 16:04:18 +0200

    edit

--- config/config.js
+++ config/config.js
@@ -7,6 +7,7 @@ const pool = createPool({
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
+    connectionLimit: 100
 })
 
 export{pool}
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import express from 'express'
-// import cors from 'cors'
+import cors from 'cors'
 import {router} from './routes/allRouter.js'
 
 const port = process.env.PORT || 9001
 const app = express()
 
-// app.use(cors({
-//     origin: 'http://localhost:8081',
-//     credentials: true
-// }))
+
+// Middleware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Request-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Expose-Headers", "Authorization");
+    next()
+})
+
+app.use(cors())
 
 app.use(express.static('public'))
 app.use(express.json())

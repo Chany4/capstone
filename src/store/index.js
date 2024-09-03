8a6fbc9732ad61bc948e1fd968ab107f70ad412d
Chany4 <chandreleighdavids04052005@gmail.com>
2024-09-03 16:17:05 +0200

    edit

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import { toast } from 'vue3-toastify';
 import "vue3-toastify/dist/index.css";
 // import { useCookies } from 'vue-cookies'
 // import router from '@/router';
-const apiURL = 'http://localhost:9001/'
+const apiURL = 'https://capstone-jm4p.onrender.com/'
 
 
 // Should you reload the page after logging in
@@ -38,19 +38,19 @@ export default createStore({
         })
       }
       },
-    async fetchUsers(context) {
-      console.log('here');
-      try {
-        const results = await (await axios.get(`${apiURL}bigTime/getUsers`)).data;
-        context.commit('setUsers', results) 
-      } catch (error) {
-        toast.error(`Ooops something `, {
-          autoClose : 3000,
-          position : 'bottom-center'
+    // async fetchUsers(context) {
+    //   console.log('here');
+    //   try {
+    //     const results = await (await axios.get(`${apiURL}bigTime/getUsers`)).data;
+    //     context.commit('setUsers', results) 
+    //   } catch (error) {
+    //     toast.error(`Ooops something `, {
+    //       autoClose : 3000,
+    //       position : 'bottom-center'
 
-        })
-      }
-      }
+    //     })
+    //   }
+    //   }
 
 
 

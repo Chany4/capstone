46e335c8d1c2b8e6e8d50351a24ea9e623c85897
Chany4 <chandreleighdavids04052005@gmail.com>
2024-09-12 11:33:59 +0200

    edit update

--- controller/usersController.js
+++ controller/usersController.js
@@ -18,22 +18,56 @@ const addUser = async (req, res) => {
     res.send('Data was successfully inserted')
 }
 
+// const updateUser = async (req, res) => {
+//     let {firstName, lastName, userAge, Gender, userRole, emailAdd, userPass, userProfile} = req.body
+//     let user = await getUserDB(req.params.id)
+
+//     firstName ? firstName = firstName : firstName = user.firstName ;
+//     lastName ? lastName = lastName : lastName = user.lastName ;
+//     userAge ? userAge = userAge : userAge = user.userAge; 
+//     Gender ? Gender = Gender : Gender = user.Gender; 
+//     userRole ? userRole = userRole : userRole = user.userRole; 
+//     emailAdd ? emailAdd = emailAdd : emailAdd = user.emailAdd; 
+//     userPass ? userPass = userPass : userPass = user.userPass
+//     userProfile ? userProfile = userProfile : userProfile = user.userProfile;
+
+//     await updateUserDB(firstName, lastName, userAge, Gender, userRole, emailAdd, userPass, userProfile, req.params.id)
+//     res.send('User info was successfully updated')
+// }
+
+
 const updateUser = async (req, res) => {
-    let {firstName, lastName, userAge, Gender, userRole, emailAdd, userPass, userProfile} = req.body
-    let user = await getUserDB(req.params.id)
+    try {
+        const { firstName, lastName, userAge, Gender, userRole, emailAdd, userPass, userProfile } = req.body;
+        const userId = req.params.id;
+        
+        // Fetch the current user data
+        const user = await getUserDB(userId);
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
 
-    firstName ? firstName = firstName : firstName = user.firstName
-    lastName ? lastName = lastName : lastName = user.lastName
-    userAge ? userAge = userAge : userAge = user.userAge
-    Gender ? Gender = Gender : Gender = user.Gender
-    userRole ? userRole = userRole : userRole = user.userRole
-    emailAdd ? emailAdd = emailAdd : emailAdd = user.emailAdd
-    userPass ? userPass = userPass : userPass = user.userPass
-    userProfile ? userProfile = userProfile : userProfile = user.userProfile
+        // Update only the fields provided in the request body
+        const updatedUser = {
+            firstName: firstName ?? user.firstName,
+            lastName: lastName ?? user.lastName,
+            userAge: userAge ?? user.userAge,
+            Gender: Gender ?? user.Gender,
+            userRole: userRole ?? user.userRole,
+            emailAdd: emailAdd ?? user.emailAdd,
+            userPass: userPass ?? user.userPass,
+            userProfile: userProfile ?? user.userProfile,
+        };
 
-    await updateUserDB(firstName, lastName, userAge, Gender, userRole, emailAdd, userPass, userProfile, req.params.id)
-    res.send('User info was successfully updated')
-}
+        // Update the user in the database
+        await updateUserDB(updatedUser, userId);
+
+        res.status(200).send('User info was successfully updated');
+    } catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).send('An error occurred while updating user info');
+    }
+};
 
 const removeUser = async (req, res) => {
     await removeUserDB(req.params.id)
